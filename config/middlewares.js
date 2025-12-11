4eb977c0fee24f632e5ceb50e74778dfd923a0da
Bribéra AGOSSOU <agossoubenie@gmail.com>
2025-12-11 20:14:49 +0100

    correction of the cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,24 +1,14 @@
+// config/middlewares.js
 module.exports = [
-  'strapi::logger',
   'strapi::errors',
-  // 'strapi::security',
-  {
-    name: 'strapi::security',
-    config: {
-      contentSecurityPolicy: {
-        directives: {
-          'img-src': ["'self'", "data:", "blob:", "res.cloudinary.com"],
-        },
-      },
-    },
-  },
+  'strapi::security',
   {
     name: 'strapi::cors',
     config: {
       origin: [
-        'http://localhost:3000',                    // Dev local
-        'https://happy-frontend-3oyr.vercel.app',   // Votre frontend Vercel
-        'https://*.vercel.app',                     // Tous les previews Vercel
+        'http://localhost:3000',
+        'https://happy-frontend-3oyr.vercel.app',
+        'https://happy-frontend-3oyr-*.vercel.app', // Autorise tous les déploiements preview
       ],
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
@@ -26,6 +16,7 @@ module.exports = [
     },
   },
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
