aeb26328e8fa5efe728e8eb314a923ebc2260a7f
Bribéra AGOSSOU <agossoubenie@gmail.com>
2025-12-18 09:39:46 +0100

    simply the ùiddlewares.js

--- config/env/production/middlewares.js
+++ config/env/production/middlewares.js
@@ -1,62 +1,10 @@
-// config/env/production/middlewares.js
 module.exports = [
   'strapi::errors',
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "connect-src": ["'self'", "https:"],
-          "img-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "market-assets.strapi.io",
-            "res.cloudinary.com",
-          ],
-          "media-src": [
-            "'self'",
-            "data:",
-            "blob:",
-            "market-assets.strapi.io",
-            "res.cloudinary.com",
-          ],
-          upgradeInsecureRequests: null,
-        },
-      },
-    },
-  },
+  'strapi::security',
   {
     name: 'strapi::cors',
     config: {
-      enabled: true,
-      headers:'*',
-      // origin: process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',' ) : ['*'],
-      // origin: ['*'], // ⚠️ À utiliser uniquement en développement
-    // OU pour production, utilisez un pattern :
-      origin: (ctx) => {
-        const allowedOrigins = [
-          'http://localhost:3000',
-          'https://happy-frontend-3oyr-cc89arqgq-briberas-projects.vercel.app'
-        ];
-        
-        const origin = ctx.request.header.origin;
-        
-        // Autoriser tous les domaines Vercel de votre projet
-        if (origin && origin.includes('vercel.app')) {
-          return origin;
-        }
-        
-        if (allowedOrigins.includes(origin)) {
-          return origin;
-        }
-        
-        return allowedOrigins[0];
-      },
-      credentials: true,
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-      keepHeaderOnError: true,
+      origin: ['*'],
     },
   },
   'strapi::poweredBy',
