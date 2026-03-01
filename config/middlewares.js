04d23dcc8e87f2f073e8abddd565e0267ef81550
Bribéra AGOSSOU <agossoubenie@gmail.com>
2026-03-01 15:40:03 +0100

    middleware config

--- config/middlewares.js
+++ config/middlewares.js
@@ -3,7 +3,7 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors', {
+  {
     name: 'strapi::cors',
     config: {
       enabled: true,
@@ -13,32 +13,10 @@ module.exports = [
         'http://localhost:3001',
         'https://ccj-frontend-3oyr.vercel.app',
         'https://ccjbenin229.vercel.app',
+        'https://ccj229.vercel.app',
       ],
-      // origin: (ctx ) => {
-      //   const allowedOrigins = [
-      //     'https://ccj-frontend-3oyr.vercel.app',
-      //     'https://ccjbenin229.vercel.app', // Add your specific domain
-      //     'http://localhost:3001',
-      //     'https://ccjbenin.vercel.app',
-      //   ];
-
-      //   const origin = ctx.request.header.origin;
-        
-      //   // Autoriser tous les domaines Vercel de votre projet
-      //   if (origin && origin.includes('vercel.app' )) {
-      //     return origin;
-      //   }
-        
-      //   if (allowedOrigins.includes(origin)) {
-      //     return origin;
-      //   }
-        
-      //   return allowedOrigins[0];
-      // },
-
-      // credentials: true,
-      // methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-      // keepHeaderOnError: true,
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
     },
   },
   'strapi::poweredBy',
